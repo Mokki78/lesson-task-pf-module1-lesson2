eec37e8b08138300f789946b8104f59f4d621de6
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2022-05-26 12:32:15 +0200

    My answers

---
+++ js/script.js
@@ -0,0 +1,72 @@
+var number1 = 2;
+var number2 = 6.5;
+
+var weekDay1 = "Monday";
+var weekDay2 = "Tuesday";
+var weekDay3 = "Wednesday";
+var weekDay4 = "Thursday";
+var weekDay5 = "Friday";
+
+var weekDays = weekDay1 + weekDay2 + weekDay3 + weekDay4 + weekDay5;
+ console.log(weekDays)
+
+ var product1 = 5;
+ var product2 = 7;
+
+ var productTotal = product1 + product2;
+ console.log(productTotal)
+
+ var productTotal = product1 - product2;
+ console.log(productTotal)
+
+ var productTotal = product1 * product2;
+ console.log(productTotal)
+
+ var productTotal = product1 / product2;
+ console.log(productTotal)
+
+ var shoppingCartEmpty = false;
+ console.log(shoppingCartEmpty)
+
+var the = 1;
+var frogs = 1;
+var are = 1;
+var angry = 1;
+
+var wordCount = the + frogs + are + angry;
+console.log(wordCount)
+
+var timeElapsed;
+var totalPrice;
+
+var number1 = 53;
+var number2 = 44.6;
+
+var pet = "donkey";
+// "string"
+
+var age = 89;
+// "number"
+
+var discountApplied = true;
+// "boolean"
+
+var timeleft;
+
+
+var headerElement = document.querySelector(".header");
+console.log(headerElement)
+
+headerElement.innerHTML = "Updating header";
+
+
+headerElement.style.color = "White";
+
+headerElement.style.fontSize = "2rem";
+
+var bodyElement = document.querySelector("body");
+console.log(bodyElement)
+
+bodyElement.style.background = "lightskyblue";
+
+
